30e3f39b5b90784e471b6fb03b2c48b920bbdefb
Thiago Pereira <thiagopereiramail@gmail.com>
2019-07-05 09:29:11 -0300

    Criando a página de contato.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:app/ui/home_page.dart';
+import 'package:app/ui/contact_page.dart';
+// import 'package:app/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomePage(),
+  home: ContactPage(),
+  debugShowCheckedModeBanner: false,
 ));
---
+++ app/lib/ui/contact_page.dart
@@ -0,0 +1,45 @@
+import 'package:app/helpers/contact.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+
+  Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if(widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {},
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
